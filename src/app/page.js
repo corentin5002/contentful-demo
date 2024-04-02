93dbb286c9e25860f0eecbb1b710f34e9e42e44f
corentin5002 <csabier@orange.fr>
2024-04-02 11:19:42 +0200

    Contentful experimentation on fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,52 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { contentfulClient } from '../component/contentfulClient';
+import RichText from "@madebyconnor/rich-text-to-jsx";
+import MangasDisplay from "@/component/MangasDisplay";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+async function fetchData(request) {
+    const response = await contentfulClient.getEntries(request);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    if (!response.items) {
+        throw new Error('No data found');
+    }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    return response.items;
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+export default async function Home() {
+
+    console.log("[debug]")
+    // fetchData('produits')
+    //     .then(data => {console.log("[NONO]",data)})
+    //     .catch(error => {console.error(error)})
+    const data = await fetchData({
+        content_type: 'manga-2',
+        locale: 'fr',
+    })
+
+
+    console.log("[debug]", data)
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                ICI CEST PARIS
+            </div>
+
+            <div className={styles.description}>
+
+                {/*{data.map((item, index) => {*/}
+
+                {/*    console.log("[debug]", index ,item)*/}
+                {/*    return ( <div key={index}>*/}
+                {/*        <h2>{item.fields.title}</h2>*/}
+                {/*        /!*<RichText richText={item.fields.description} />*!/*/}
+                {/*    </div>)*/}
+                {/*})}*/}
+                <MangasDisplay data={data} />
+            </div>
+
+        </main>
+    );
 }
