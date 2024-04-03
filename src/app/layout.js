5100332cc427d59e9496c2c3011b69d0d91b0046
corentin5002 <csabier@orange.fr>
2024-04-03 15:48:13 +0200

    Add vercel speedinsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,9 @@
-export default function RootLayout({ children, params }) {
-    return children;
+import {SpeedInsights} from "@vercel/speed-insights/next";
+
+export default function RootLayout({children, params}) {
+    return (
+        <>
+            {children}
+        </>
+    );
 }
